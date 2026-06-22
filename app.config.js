604c341852db5b470c7886dcbc75bf195ba27c54
OkatamiracleDev <okatamiracle.dev@gmail.com>
2026-06-22 10:49:25 -0500

    fix file config

--- app.config.js
+++ app.config.js
@@ -2,22 +2,22 @@ module.exports = ({ config }) => ({
   ...config,
   name: "UniHub",
   slug: "unihub-mobile",
-  scheme: "unihub",
+  scheme: "UniHub Events",
   version: "0.1.0",
   orientation: "default",
   userInterfaceStyle: "automatic",
   android: {
-    package: "click.tryunihub.mobile"
+    package: "click.tryunihub.mobile",
   },
   extra: {
     githubRepo: "Unihub-events-app/unihub-mobile",
     eas: {
-      projectId: "5e978b89-736e-45ec-89d6-f42b3c53b922"
+      projectId: "5e978b89-736e-45ec-89d6-f42b3c53b922",
     },
     API_URL:
       process.env.EXPO_PUBLIC_API_URL ||
       process.env.NEXT_PUBLIC_API_URL ||
-      "https://try-unihub-production-8a88.up.railway.app"
+      "https://try-unihub-server.pxxl.click",
   },
-  plugins: ["expo-router", "expo-image", "expo-status-bar", "expo-font"]
+  plugins: ["expo-router", "expo-image", "expo-status-bar", "expo-font"],
 });
